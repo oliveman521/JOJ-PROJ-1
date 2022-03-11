4e6e13a91155fb8c53b474a381ef271d046457c9
Oliver Philbrick <oliveman521@gmail.com>
2022-03-11 17:17:40 -0500

    Player Controller Tweaks

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5;
-    
+    private Rigidbody2D rb;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -19,7 +19,16 @@ public class PlayerController : MonoBehaviour
         playerInputs.x = Input.GetAxisRaw("Horizontal");
         playerInputs.y = Input.GetAxisRaw("Vertical");
 
-        transform.position += (Vector3)playerInputs.normalized * speed * Time.deltaTime;
 
+        rb.velocity = playerInputs.normalized * speed;
+
+        if(rb.velocity.x > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, transform.rotation.z);
+        }
+        else if (rb.velocity.x < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, transform.rotation.z);
+        }
     }
 }
